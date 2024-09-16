2cfc36836a7bdbe5e90379a538a093d634afda3c
Santayo Kundu <141609844+SantayogithubIT@users.noreply.github.com>
2024-09-16 06:18:06 +0000

    Data Types of JS

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser
+// DATA TYPES===>
+    // number=> 2 to the power 53
+// bigInt
+// string=""
+// null
+// undefined
+// symbol
+// boolean= true/false
+
+let age=18
+let temp;
+
+console.log(typeof temp); //undefined
+console.log(typeof null); // object.
+
+
