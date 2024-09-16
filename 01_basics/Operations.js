35500a335d5aa04bc5e938a29779970c9fa579e6
Santayo Kundu <141609844+SantayogithubIT@users.noreply.github.com>
2024-09-16 07:34:42 +0000

    operations of data types

---
+++ 01_basics/Operations.js
@@ -0,0 +1,18 @@
+let value=254
+let neValue= -value
+
+console.log(neValue);
+
+//Basic Opetarions==>>
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);
+console.log("***************");
+
+//true to 1 conversions
+console.log(+true);   //true==1
+console.log(+"");     //""==false==0
+
