bc2c7861d16a005365f847109922b8e5424faff1
Santayo Kundu <141609844+SantayogithubIT@users.noreply.github.com>
2024-09-16 05:45:44 +0000

    Variables of Js Let, Var

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountID="430422010001"
+let accountMail="santayo@gmail.com"
+let accountPass="12364795"
+let accountName="Santayo"
+
+//console.log(accountCity);
+
+//prefer not to use Var, because of block of code//
+accountName="Santayo Kundu"
+
+console.table([accountID, accountMail, accountPass, accountName])   //forms a table for given input of acc details.\
+
